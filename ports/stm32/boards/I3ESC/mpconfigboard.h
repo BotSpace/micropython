9534b5b5eeeaf423701b4b7cede6abe2c0a75dee
Jacob Williams <47766188+BotSpace@users.noreply.github.com>
2024-08-22 02:20:14 -0400

    compilation succeeds! but 431 does not have enough flash :(

--- ports/stm32/boards/I3ESC/mpconfigboard.h
+++ ports/stm32/boards/I3ESC/mpconfigboard.h
@@ -51,18 +51,18 @@
 //#define MICROPY_HW_UART_REPL_BAUD   (115200)
 
 // I2C buses
-#define MICROPY_HW_I2C1_SCL         (pin_B8) // A13, A15, B8 (=boot0)
-#define MICROPY_HW_I2C1_SDA         (pin_B9) // A14, B7, B9
+//#define MICROPY_HW_I2C1_SCL         (pin_B8) // A13, A15, B8 (=boot0)
+//#define MICROPY_HW_I2C1_SDA         (pin_B9) // A14, B7, B9
 // #define MICROPY_HW_I2C2_SCL       (pin_)   // A9, C4
 // #define MICROPY_HW_I2C2_SDA       (pin_)   // A8, F0
 // #define MICROPY_HW_I2C3_SCL       (pin_)   // A8, C8
 // #define MICROPY_HW_I2C3_SDA       (pin_)   // B5, C9, C11
 
 // SPI
-#define MICROPY_HW_SPI1_NSS         (pin_A4) // A4, A15 (Nucleo64 specifies B6 as pin CS, must be done as GPIO, not as AF)
-#define MICROPY_HW_SPI1_SCK         (pin_A5) // A5 (LED), B3 (SWO)
-#define MICROPY_HW_SPI1_MISO        (pin_A6) // A6, B4
-#define MICROPY_HW_SPI1_MOSI        (pin_A7) // A7, B5
+//#define MICROPY_HW_SPI1_NSS         (pin_A4) // A4, A15 (Nucleo64 specifies B6 as pin CS, must be done as GPIO, not as AF)
+//#define MICROPY_HW_SPI1_SCK         (pin_A5) // A5 (LED), B3 (SWO)
+//#define MICROPY_HW_SPI1_MISO        (pin_A6) // A6, B4
+//#define MICROPY_HW_SPI1_MOSI        (pin_A7) // A7, B5
 // #define MICROPY_HW_SPI2_NSS       (pin_)   // B12, D15, F0
 // #define MICROPY_HW_SPI2_SCK       (pin_)   // B13, F1, F9, F10
 // #define MICROPY_HW_SPI2_MISO      (pin_)   // A10, B14
@@ -73,10 +73,10 @@
 // #define MICROPY_HW_SPI3_MOSI      (pin_)   // B5, C12
 
 // USRSW is pulled low. Pressing the button makes the input go high.
-//#define MICROPY_HW_USRSW_PIN        (pin_C13)
-//#define MICROPY_HW_USRSW_PULL       (GPIO_NOPULL)
-//#define MICROPY_HW_USRSW_EXTI_MODE  (GPIO_MODE_IT_RISING)
-//#define MICROPY_HW_USRSW_PRESSED    (1)
+#define MICROPY_HW_USRSW_PIN        (pin_A0) //TEMP_2
+#define MICROPY_HW_USRSW_PULL       (GPIO_PULLUP)
+#define MICROPY_HW_USRSW_EXTI_MODE  (GPIO_MODE_IT_FALLING)
+#define MICROPY_HW_USRSW_PRESSED    (0)
 
 // LEDs
 #define MICROPY_HW_LED1             (pin_B3)    // red JMOD
